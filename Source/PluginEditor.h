285b0683eb1ac3dbb87966df719310478f6a46b2
BarbodH <habibi.k.barbod@gmail.com>
2025-11-01 03:22:14 -0400

    Apply ClangFormat

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,36 +1,24 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
 
-#include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include <JuceHeader.h>
 
-//==============================================================================
-/**
-*/
-class A2StarterAudioProcessorEditor  : public juce::AudioProcessorEditor, private juce::Slider::Listener
-{
+class A2StarterAudioProcessorEditor : public juce::AudioProcessorEditor,
+                                      private juce::Slider::Listener {
 public:
-    A2StarterAudioProcessorEditor (A2StarterAudioProcessor&);
-    ~A2StarterAudioProcessorEditor() override;
+  A2StarterAudioProcessorEditor(A2StarterAudioProcessor &);
+  ~A2StarterAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+  void paint(juce::Graphics &) override;
+  void resized() override;
 
 private:
-    void sliderValueChanged (juce::Slider* slider) override;
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    A2StarterAudioProcessor& audioProcessor;
+  void sliderValueChanged(juce::Slider *slider) override;
+  // This reference is provided as a quick way for your editor to
+  // access the processor object that created it.
+  A2StarterAudioProcessor &audioProcessor;
 
-    juce::Slider volumeSlider; // [1]
-    
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (A2StarterAudioProcessorEditor)
+  juce::Slider volumeSlider; // [1]
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(A2StarterAudioProcessorEditor)
 };
